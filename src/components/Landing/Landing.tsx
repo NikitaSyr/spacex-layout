70f829d9fc0aa4ccbc57e7dfb8c4d62a9545bf0f
NikitaSyr <galaxynik7@gmail.com>
2022-04-07 19:26:01 +0800

    5. Landing page finished

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -4,17 +4,28 @@ import s from "./Landing.module.css";
 import TravelButton from "./../../assets/img/StartButton.png";
 import {NavLink} from "react-router-dom";
 
+interface BoxItemType {
+    topText: string
+    middleText: string
+    bottomText: string
+}
 
 const Landing = () => {
-    // eslint-disable-next-line no-restricted-globals
-    // let userScreenHeight = screen.height;
-
+    const BoxItem: React.FC<BoxItemType> = ({topText, middleText, bottomText}) => {
+        return (
+            <div className={s.box__item}>
+                <div className={s.item__topText}>{topText}</div>
+                <div className={s.item__middleText}>{middleText}</div>
+                <div className={s.item__bottomText}>{bottomText}</div>
+            </div>
+        )
+    }
     return (
         <div className={`${s.landing} ${s.landing__background}`}>
             <div className={s.landing__column}>
                 <div className={s.landing__UpperRow}>
                     <div className={s.landing__heading}>
-                        <div className={`${s.heading__text} ${s.text__widthAdaptive} ${s.text__heightAdaptive}`}>
+                        <div className={`${s.heading__text} ${s.text__widthAdaptive}`}>
                             <div className={s.text__travel}>Путешествие</div>
                             <div className={s.text__redPlanet}>на красную планету</div>
                         </div>
@@ -31,11 +42,33 @@ const Landing = () => {
                 </div>
             </div>
             <div className={s.landing__column}>
-
+                <div className={s.landing__UpperRow}>
+                    <div className={s.landing__benefits}>
+                        <div className={`${s.benefits__box} ${s.box__topLeft}`}>
+                            <BoxItem topText={"мы"} middleText={"1"} bottomText={"на рынке"}/>
+                        </div>
+                        <div className={`${s.benefits__box} ${s.box__topRight}`}>
+                            <BoxItem topText={"гарантируем"} middleText={"50%"} bottomText={"безопасность"}/>
+                        </div>
+                    </div>
+                </div>
+                <div className={s.landing__LowerRow}>
+                    <div className={s.landing__benefits}>
+                        <div className={`${s.benefits__box} ${s.box__bottomLeft}`}>
+                            <div className={s.box__item}>
+                                <div className={s.item__topText}>календарик за</div>
+                                <div className={s.item__middleText}>
+                                    <span>2001</span><span className={s.item__middleTextSmall}>г.</span>
+                                </div>
+                                <div className={s.item__bottomText}>в подарок</div>
+                            </div>
+                        </div>
+                        <div className={`${s.benefits__box} ${s.box__bottomRight}`}>
+                            <BoxItem topText={"путешествие"} middleText={"597"} bottomText={"дней"}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/*<div className={s.landing__row}>*/}
-            {/*    gg*/}
-            {/*</div>*/}
         </div>
     )
 }
